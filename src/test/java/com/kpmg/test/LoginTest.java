5f3b7e4995d43e001b82fed1b201a2fd5eb73b2e
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-08 22:14:02 +0530

    Added dataproviders

--- src/test/java/com/kpmg/test/LoginTest.java
+++ src/test/java/com/kpmg/test/LoginTest.java
@@ -1,29 +1,37 @@
 package com.kpmg.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.kpmg.base.AutomationWrapper;
+import com.microsoft.playwright.options.SelectOption;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 
 /**
  * This class test the login functionality. 
  */
 public class LoginTest extends AutomationWrapper {
+	
+//	admin, pass, 18, Calendar
+//	accountant,accountant, 18, Calendar
 
 	@Test
-	public void validLoginTest()
-	{
+	public void validLoginTest(String username,String password,String languageValue,String expectedValue) {
 		page.locator("xpath=//input[@id='authUser']").fill("physician");
-		//password - physician
-		//click on login
-		//assert - calendar menu
+		page.locator("xpath=//input[@id='clearPass']").fill("physician");
+		page.locator("xpath=//select[@name='languageChoice']").selectOption(new SelectOption().setValue("18"));
+		page.locator("xpath=//button[@id='login-button']").click();
+		assertThat(page.locator("xpath=//span[text()='Calendar']")).hasText("Calendar");	
 	}
-	
+
 	@Test
-	public void invalidLoginTest()
-	{
-		page.locator("xpath=//input[@id='authUser']").fill("john123");
-		//password - physician
-		//click on login
-		//assert - calendar menu
+	public void invalidLoginTest() {
+		page.locator("xpath=//input[@id='authUser']").fill("admin");
+		page.locator("xpath=//input[@id='clearPass']").fill("admin123");
+		page.locator("xpath=//select[@name='languageChoice']").selectOption(new SelectOption().setValue("18"));
+		page.locator("xpath=//button[@id='login-button']").click();
+		String actualError=page.locator("xpath=//p[contains(text(),'Invalid username')]").innerText(); 
+		Assert.assertTrue(actualError.contains("Invalid username233"),"Actual message does not contain expected text. Actual message -"+actualError);
 	}
 }
---
+++ src/test/java/com/kpmg/test/ZDemo1Test.java
@@ -0,0 +1,34 @@
+package com.kpmg.test;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Not part of the framework
+ */
+public class ZDemo1Test {
+
+	@DataProvider
+	public String[][] getValidData() 
+	{
+		String[][] data = new String[3][2];
+		//i --> number of testcase
+		//j --> number of parameters
+
+		data[0][0] = "john";
+		data[0][1] = "john123";
+
+		data[1][0] = "saul";
+		data[1][1] = "saul123";
+
+		data[2][0] = "kim";
+		data[2][1] = "kim123";
+		
+		return data;
+	}
+
+	@Test(dataProvider = "getValidData")
+	public void validTest(String username, String password) {
+		System.out.println("valid login" + username + password);
+	}
+}
